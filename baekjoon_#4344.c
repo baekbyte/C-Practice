5b4ed3a102cb2f3e31e5f8d80399ee89eb89e7c7
Apple-pro1 <73371470+Apple-pro1@users.noreply.github.com>
2021-01-30 10:27:11 +0900

    Create baekjoon_#4344.c

---
+++ baekjoon_#4344.c
@@ -0,0 +1,45 @@
+#include<stdio.h> 
+#include<string.h>
+#include<stdlib.h>
+int main(void) { 
+
+  int C;
+  int i, j;
+  int N;
+  int *score;
+  int sum=0;
+  double average = .0;
+
+
+  scanf("%d", &C);
+
+  for(i=0; i<C; i++){
+    scanf("%d", &N);
+
+    score = (int *)malloc(N*sizeof(int));
+    if (score == NULL){
+      printf("Malloc Error");
+      exit(1);
+    }
+
+    for(j=0; j<N; j++){
+      scanf("%d", &score[j]);
+      sum += score[j];
+    }
+
+    average= (double)sum /N;
+    int count = 0;
+    for (int j=0; j<N; j++){
+      if(average<score[j]){
+        count++;
+      }
+    }
+    printf("%.3f%%\n", (double)count*100/N);
+  }
+
+
+return 0;
+}
+
+
+
